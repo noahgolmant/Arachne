1711c37d80109c6f92a2c73d1bb5c04f21a53d42
Noah Golmant <noah.golmant@gmail.com>
2016-01-23 19:24:13 -0800

    add bucket filtering for likely similar substrings that form rule set

--- dustbuster/dustbuster.scala
+++ dustbuster/dustbuster.scala
@@ -7,8 +7,8 @@ object DustBuster {
        text after that substr and (*TODO*) the ratio
        of the size_range to the doc_sketch for the URL */
     def possibleSubstrings(line:String) : List[Tuple3[String,String,String]] = {
-        val len = line.length() - 1
-        var substrings = List[Tuple3[String,String,String]]()
+        val len = line.length() - 1;
+        var substrings = List[Tuple3[String,String,String]]();
         for ( i <- 1 to len) {
             /* for each substring in line of length i, get prefix and suffix */
             for ( j <- 0 to len-i) {
@@ -21,6 +21,27 @@ object DustBuster {
         return substrings;
     }
 
+    val BucketOverflowSize = 1000;
+    val MaxSubstringLengthDiff = 4;
+
+    def filterBuckets((prefix, suffix):Tuple2[String,String], substrings:Iterable[String]) = {
+        
+        def likelySimilar(substr1: String, substr2: String) {
+            /* TODO use size ratio or document sketch */
+            return Math.abs(substr1.length() - substr2.length()) < MaxSubstringLengthDiff;
+        }
+        
+        if (substrings.size == 1 || substrings.size > BucketOverflowSize)
+            return None;
+
+        for ( substr1 <- substrings ) {
+            for ( substr2 <- substrings) {
+                if (likelySimilar(substr1, substr2))
+                    yield (substr1, substr2)
+            }
+        }
+    }
+
     def main(args:Array[String]) = {
         val sc = new SparkContext();
         val urlData = sc.parallelize(args(0));
@@ -29,9 +50,9 @@ object DustBuster {
         val substringTable = urlData.flatMap(possibleSubstrings);
         /* group all substrings into buckets by (prefix, suffix) tuple */
         val buckets = substringTable
-            .map((substr, prefix, suffix) => ((prefix, suffix), substr))
+            .map((substr, prefix, suffix) => ((prefix, suffix), substr)
             .groupByKey();
-        
-
+           
+        val instanceTable = buckets.map(filterBuckets).filter(_.nonEmpty);
     }
 }
