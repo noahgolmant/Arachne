b1f5c6c847dffe20f65489f74788194be33388ca
Ravi <rpandya922@berkeley.edu>
2016-01-23 18:50:55 -0800

    Added javadocs to URLQueue Class

--- src/main/java/com/arachne/URLQueue.java
+++ src/main/java/com/arachne/URLQueue.java
@@ -4,34 +4,76 @@ package com.arachne;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+/**
+ * @class URLQueue
+ * @description Represents the list of recently processed URLs by all the nodes
+ * @author Ravi Pandya
+ */
 public class URLQueue{
-	
+	/**
+	 * A linked list representation of the recently processed URLs
+	 */
 	private LinkedList<String> urls = new LinkedList<String>();
+	
+	/**
+	 * A HashMap with each distinct domain paired with the number of times it has been accessed
+	 * recently
+	 */
 	private HashMap<String, Integer> distribution = new HashMap();
+	
+	/**
+	 * The maximum length of the queue, or what will be considered 'recent'
+	 */
 	private int maxLength;
+	
+	/**
+	 * The current length of the queue
+	 */
 	private int length;
 	
+	/**
+	 * Static method to extract a domain name from any given URL
+	 * @param url The URL to be processed
+	 * @return domain The domain of the URL passed in
+	 */
+	public static String getDomain(String url){
+		return url;
+	}
+	
+	/**
+	 * Constructor for a new URLQueue taking in no previously processed URLs
+	 * @param maxLength What will be considered 'recent' for this queue
+	 */
 	public URLQueue(int maxLength){
 		this.length = 0;
 		this.maxLength = maxLength;
 	}
 	
+	/**
+	 * Constructor 
+	 * @param maxLength What will be considered 'recent' for this queue
+	 * @param urls A LinkedList of URLs that have already been processed
+	 */
 	public URLQueue(int maxLength, LinkedList<String> urls){
 		this.urls = urls;
 		this.length = urls.size();
 		this.maxLength = maxLength;
 	}
 	
-	public String getDomain(String url){
-		return url;
-	}
-	
+	/**
+	 * Method for adding a new URL to the list of recently processed URLs, pops the last 
+	 * element off of the queue if the queue has reached its maximum length
+	 * @param url the URL that has just been processed and must be added to the stack
+	 */
 	public void add(String url){
-		String domain = this.getDomain(url);
+		String domain = getDomain(url);
 		
 		if(this.length >= maxLength){
+			String last = this.urls.getLast();
+			this.distribution.put(last, this.distribution.get(last)-1);
 			this.urls.removeLast();
 		}
+		
 		this.urls.add(0, url);
 		this.length = urls.size();
 		
