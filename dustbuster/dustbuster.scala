1a286285fc4a2b8fc4f40dfcfc813d1cc7e2698f
Noah Golmant <noah.golmant@gmail.com>
2016-01-26 22:14:40 -0800

    nearly done with initial rule detection; syntax errors left ot fix

--- dustbuster/dustbuster.scala
+++ dustbuster/dustbuster.scala
@@ -1,58 +1,70 @@
 import org.apache.spark.SparkContext
+import scala.math.min
 
 object DustBuster {
 
-    /* Maps string to tuples where each tuple contains
-       the substring, the text before that substr, the
-       text after that substr and (*TODO*) the ratio
-       of the size_range to the doc_sketch for the URL */
-    def possibleSubstrings(line:String) : List[Tuple3[String,String,String]] = {
-        val len = line.length() - 1;
-        var substrings = List[Tuple3[String,String,String]]();
-        for ( i <- 1 to len) {
-            /* for each substring in line of length i, get prefix and suffix */
-            for ( j <- 0 to len-i) {
+    def detectLikelyRules(urlData:RDD[String]) : RDD[Tuple2[String, Integer]] = {
+
+        val MaximumSubstringSize = 12;
+
+        /* Maps string to tuples where each tuple contains
+        the substring, the text before that substr, the
+        text after that substr and (*TODO*) the ratio
+        of the size_range to the doc_sketch for the URL */
+        def possibleSubstrings(line:String) : List[Tuple3[String,String,String]] = {
+            val len = min(line.length() - 1, MaximumSubstringSize);
+            for {
+                i <- 1 to len
+                j <- 0 to len-i
+            } yield {
+                /* for each substring in line of length i, get prefix and suffix */
                 val substr = line.substring(j, j+i);
                 val prefix = line.substring(0, j);
                 val suffix = line.substring(j+i, len);
-                substrings = (substr, prefix, suffix) :: substrings;                   
+                (substr, prefix, suffix);
             }
         }
-        return substrings;
-    }
 
-    val BucketOverflowSize = 1000;
-    val MaxSubstringLengthDiff = 4;
+        val BucketOverflowSize = 1000;
+        val MaxSubstringLengthDiff = 4;
 
-    def filterBuckets((prefix, suffix):Tuple2[String,String], substrings:Iterable[String]) = {
+        def filterBuckets(envelope:Tuple2[String,String], substrings:Iterable[String]) : Tuple2[Tuple2[String,String],Tuple2[String,String]] = {
         
-        def likelySimilar(substr1: String, substr2: String) {
-            /* TODO use size ratio or document sketch */
-            return Math.abs(substr1.length() - substr2.length()) < MaxSubstringLengthDiff;
-        }
+            def likelySimilar(substr1: String, substr2: String) : Boolean = {
+                /* TODO use size ratio or document sketch */
+                /* Or use hamming/levenshtein distance here */
+                return Math.abs(substr1.length() - substr2.length()) < MaxSubstringLengthDiff;
+            }
+            if (substrings.size == 1 || substrings.size > BucketOverflowSize)
+                return None;
         
-        if (substrings.size == 1 || substrings.size > BucketOverflowSize)
-            return None;
-
-        for ( substr1 <- substrings ) {
-            for ( substr2 <- substrings) {
+            val (prefix, suffix) = envelope;
+            val substringsLst = new Array[String](substrings.size);
+            substrings.copyToArray(substringsLst);
+            for {
+                substr1 <- substringsLst
+                substr2 <- substringsLst
+            } yield {
                 if (likelySimilar(substr1, substr2))
-                    yield (substr1, substr2)
+                    ((substr1,substr2),(prefix,suffix))
             }
         }
+
+        return urlData.flatMap(possibleSubstrings) /* get all possible substrings for each url */
+                      /* create buckets based on unique prefix suffix pairs */
+                      .map((substr,prefix,suffix) => ((prefix, suffix), substr)).groupByKey()
+                      /* create potential rules and group by substring pairs (a,b) where one rule = a |--> b transform */
+                      .map(filterBucket).filter(_.nonEmpty).groupByKey()
+                      /* calculate rule support and sort by best rule */
+                      .mapValues(envelopes => envelopes.size)
+                      .sortBy(_._2); /* sort by second element of (rule, support) tuple i.e. support count */
     }
 
     def main(args:Array[String]) = {
         val sc = new SparkContext();
-        val urlData = sc.parallelize(args(0));
+        var urlData = sc.parallelize(args(0));
         urlData = urlData.map(line => "^" + line + "$"); /* tokenize urls */
-        /* collect possible substrings of each URL */
-        val substringTable = urlData.flatMap(possibleSubstrings);
-        /* group all substrings into buckets by (prefix, suffix) tuple */
-        val buckets = substringTable
-            .map((substr, prefix, suffix) => ((prefix, suffix), substr)
-            .groupByKey();
-           
-        val instanceTable = buckets.map(filterBuckets).filter(_.nonEmpty);
+        
+        var likelyRules = detectLikelyRules(urlData);
     }
 }
